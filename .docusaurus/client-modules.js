08f5823599ec8d762031e5b79cd5f1b53611a5c9
nickignacio <nicholas.ignacio@outlook.com>
2024-03-12 13:49:37 -0400

    Deploy website - based on f5dca3c1e8e111c2d08dbccca3bfe5d80b21700f

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/nickignacio/Desktop/nickignacio.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/nickignacio/Desktop/nickignacio.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/nickignacio/Desktop/nickignacio.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/nickignacio/Desktop/nickignacio.github.io/src/css/custom.css"),
-];
